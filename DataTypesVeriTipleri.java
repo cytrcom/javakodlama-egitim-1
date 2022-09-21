5829ff5c004402d6d851b804b3ed39a3d55a85c6
cytrcom <33841594+cytrcom@users.noreply.github.com>
2022-09-21 14:32:20 +0300

    Create DataTypesVeriTipleri.java

---
+++ DataTypesVeriTipleri.java
@@ -0,0 +1,9 @@
+//DATA TYPES VERİ TİPLERİ
+/*
+int myNum = 5;               // Integer (whole number)
+float myFloatNum = 5.99f;    // Floating point number
+char myLetter = 'D';         // Character
+boolean myBool = true;       // Boolean
+String myText = "Hello";     // String
+*/
+//Primiteve Types = İlkel Tip (En temel veri tipleri)
