e9402d89177519a8eafe76273f3076206a26eb64
cytrcom <33841594+cytrcom@users.noreply.github.com>
2022-09-21 16:20:16 +0300

    Create forLoopDonguler.java

---
+++ forLoopDonguler.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String[] args) {
+   
+   //Java For Loop | For Döngüsü 
+
+	// 1 den (1 dahil) 10 kadar (10 dahil) olan sayıları yazdırma
+    
+   for (int i=1;i<=10;i++) 
+   /*
+   i=1 ifadesi i'yi 1 e eşitledik (Sayaç Tanıma)
+   i<10 ifadesi şart kısmı (Şart)
+   i++ veya i=i+1 ifadesi i'yi birer birer artır demektir. i-- de birer birer azaltır. (Artış Değeri)
+   */
+   {
+   System.out.println(i);
+   //i 1 den başlar 10 olunca kadar bu satır ile for satırı arasında işlem devam eder. buna döngü diyoruz.
+   }
+   }
+}
