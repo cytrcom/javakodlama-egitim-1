28cedbe22ed510bf5430ab915edd9506d74833e3
cytrcom <33841594+cytrcom@users.noreply.github.com>
2022-09-21 08:45:54 +0300

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
+    System.out.println("Merhaba Dünya");
+    System.out.println("Merhaba Java :)");
   }
 }
