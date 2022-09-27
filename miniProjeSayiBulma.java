782bf7921c9b0c43c4dc13fd1d906a8c94a204f3
cytrcom <33841594+cytrcom@users.noreply.github.com>
2022-09-27 23:34:02 +0300

    Create miniProjeSayiBulma.java

---
+++ miniProjeSayiBulma.java
@@ -0,0 +1,22 @@
+public class miniProjeSayiBulma {
+    public static void main(String[] args) {
+
+        int[] sayilar = new int[]{1, 2, 5, 7, 9, 0};
+        int aranacak = 9;
+        boolean varMi = false;
+
+        for (int sayi : sayilar) {
+            if (sayi == aranacak) {
+                varMi = true;
+                break;
+            }
+        }
+        if (varMi) {
+            System.out.println("Sayı mevcuttur");
+        } else {
+            System.out.println("Sayı mevcut değildir");
+        }
+
+    }
+
+}
